6ec2db7321a91ce7b52973dec7193c88f5d3bd08
Håkon <haakonu97@gmail.com>
2022-11-25 20:42:23 +0100

    refactor: Possible solution for onFetching state reset issue using display contents/none toggling

--- src/RTKLoader.tsx
+++ src/RTKLoader.tsx
@@ -2,6 +2,14 @@ import { SerializedError } from "@reduxjs/toolkit";
 import * as React from "react";
 import { CustomLoaderProps } from "./types";
 
+const hiddenWrapperStyle = {
+  display: "none",
+};
+
+const shownWrapperStyle = {
+  display: "contents",
+};
+
 export function RTKLoader<T>(
   props: CustomLoaderProps<T>
 ): React.ReactElement {
@@ -9,6 +17,11 @@ export function RTKLoader<T>(
     props.query.isLoading || props.query.isUninitialized;
   const hasError = props.query.isError && props.query.error;
   const isFetching = props.query.isFetching;
+
+  const wrapperStyle =
+    isFetching && props.onFetching
+      ? hiddenWrapperStyle
+      : shownWrapperStyle;
 
   if (shouldLoad) {
     return props.onLoading ?? <React.Fragment />;
@@ -22,10 +35,6 @@ export function RTKLoader<T>(
     );
   }
 
-  if (isFetching && props.onFetching) {
-    return props.onFetching;
-  }
-
   if (props.query.data !== undefined) {
     const prepend = isFetching
       ? props.whileFetching?.prepend ?? null
@@ -33,12 +42,21 @@ export function RTKLoader<T>(
     const append = isFetching
       ? props.whileFetching?.append ?? null
       : null;
+    const onFetchingComponent =
+      isFetching && props.onFetching ? props.onFetching : null;
+
     const componentWithData = props.onSuccess(props.query.data);
 
     return (
       <>
         {prepend}
-        {componentWithData}
+        <div
+          style={wrapperStyle}
+          aria-hidden={isFetching ? true : false}
+        >
+          {componentWithData}
+        </div>
+        {onFetchingComponent}
         {append}
       </>
     );
